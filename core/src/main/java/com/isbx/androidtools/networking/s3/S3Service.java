3dd6e38abb1e848d84667e5a2ddf98252f09b427
Jaype Sison <dev.jpsison@gmail.com>
2018-11-30 22:45:25 +0800

    Use Multipart

--- core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
+++ core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
@@ -7,13 +7,13 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 
 public interface S3Service {
 
     @POST("/")
-    @FormUrlEncoded
+    @Multipart
     Call<ResponseBody> uploadFile(
         @Field("key") String key,
         @Field("AWSAccessKeyId") String accessKeyId,
