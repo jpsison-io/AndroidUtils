dc9b88b6d5110372b58bad2fade67d31ce456dd9
Jaype Sison <dev.jpsison@gmail.com>
2018-12-01 00:47:07 +0800

    Use Put

--- core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
+++ core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
@@ -10,11 +10,12 @@ import retrofit2.http.Field;
 import retrofit2.http.Header;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Part;
 
 public interface S3Service {
 
-    @POST("/")
+    @PUT("/")
     @Multipart
     Call<ResponseBody> uploadFile(
         @Part("key") RequestBody key,
