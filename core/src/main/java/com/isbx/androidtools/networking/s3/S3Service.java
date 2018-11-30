fa3e95ea96dd8fe8b6aa65ee330e6cc126ecf4eb
Jaype Sison <dev.jpsison@gmail.com>
2018-11-30 22:20:46 +0800

    Remove Multipart

--- core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
+++ core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
@@ -8,12 +8,10 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 
 public interface S3Service {
 
-    @Multipart
     @POST("/")
     @FormUrlEncoded
     Call<ResponseBody> uploadFile(
