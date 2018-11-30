8dac429f83daffab34511d0279a9983149be96ee
Jaype Sison <dev.jpsison@gmail.com>
2018-11-30 23:36:56 +0800

    Use Part as Params

--- core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
+++ core/src/main/java/com/isbx/androidtools/networking/s3/S3Service.java
@@ -6,6 +6,7 @@ import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.Header;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -15,13 +16,13 @@ public interface S3Service {
     @POST("/")
     @Multipart
     Call<ResponseBody> uploadFile(
-        @Field("key") String key,
-        @Field("AWSAccessKeyId") String accessKeyId,
-        @Field("policy") String policy,
-        @Field("signature") String signature,
-        @Field("success_action_status") Integer successStatus,
-        @Field("acl") String acl,
+        @Part("key") String key,
+        @Part("AWSAccessKeyId") String accessKeyId,
+        @Part("policy") String policy,
+        @Part("signature") String signature,
+        @Part("success_action_status") Integer successStatus,
+        @Part("acl") String acl,
         @Part MultipartBody.Part filePart,
-        @Field("Content-Type") String contentType
+        @Part("Content-Type") String contentType
     );
 }
